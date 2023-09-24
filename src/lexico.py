0ddf5fa8fc6dc89f7575af911af2c95708179c6a
Rafael Alves <rafael.alv334@gmail.com>
2023-09-24 20:49:45 -0300

    Início do analisador léxico

--- src/lexico.py
+++ src/lexico.py
@@ -1,4 +1,29 @@
+import tabela
+
 arquivo = None
+nome_id = None
+
+estados_finais = [
+    '2', '4', '5', '7', '8', '10', '11', '12', '13', '14', '15',
+    '17', '20', '22', '24', '27', '31', '32', '33', '35', '36',
+    '37', '38', '40'
+]
+
+tabela_transicao = {
+    '1': {
+        '=': '2',
+        '>': '3',
+        '<': '6',
+        '!': '9',
+        '+': '11',
+        '-': '12',
+        '*': '13',
+        '/': '14',
+        '^': '15',
+        '{': '16',
+        "'": '18'
+    }
+}
 
 
 def abre_arquivo(caminho: str) -> None:
@@ -15,18 +40,118 @@ def abre_arquivo(caminho: str) -> None:
     except IOError as e:  # Problemas de entrada/saida gerais
         print(f'Erro de E/S: {e}')
         raise
+    except Exception as e:
+        print(f'Erro desconecido: {e}')
+        raise
 
 
 def lookhead() -> None:
     arquivo.seek(arquivo.tell() - 1, 0)
 
 
-# Retorna um token por vez
-def getToken():
+def setInt() -> None:
+    global nome_id
+
+    item = tabela.tabela_de_simbolos.get(nome_id)
+
+    if item is None:
+        tabela.tabela_de_simbolos[nome_id] = ('numero', int(nome_id), 'int')
+
+    # TODO: RETORNAR OS VALORES
+
+
+def setFrac() -> None:
+    global nome_id
+
+    item = tabela.tabela_de_simbolos.get(nome_id)
+
+    if item is None:
+        tabela.tabela_de_simbolos[nome_id] = ('numero', float(nome_id), 'float')
+
+    # TODO: RETORNAR OS VALORES
+
+
+def setExp() -> None:
+    global nome_id
+
+    item = tabela.tabela_de_simbolos.get(nome_id)
+
+    if item is None:
+        ...
+
+    # TODO: RETORNAR OS VALORES
+
+
+def setId() -> None:
+    global nome_id
+
+    item = tabela.tabela_de_simbolos.get(nome_id)
+
+    if item is None:
+        tabela.tabela_de_simbolos[item] = ('id', '', '')
+        # TODO: RETORNAR OS VALORES
+    else:
+        if item[0] == 'id':  # Não está reservado, mas está presente na tabela
+            ...
+            # TODO: RETORNAR OS VALORES
+        else:  # É reservado
+            ...
+            # TODO: RETORNAR OS VALORES
+
+
+def setChar() -> None:
     ...
+
+
+def prox_char() -> str:
+    global arquivo
+
+    char = arquivo.read(1)
+
+    if char:
+        return char
+    else:
+        return 'EOF'
+
+
+def final(estado: str) -> bool:
+    if estado in estados_finais:
+        return True
+    else:
+        return False
+
+
+def move(estado: str, char: str) -> str:
+    if estado in tabela_transicao and char in tabela_transicao[estado]:
+        return tabela_transicao[estado][char]
+    else:
+        return '-1'
+
+
+def getToken() -> None:
+    global nome_id
+
+    print_estado = True
+    estado = "1"
+    char = prox_char()
+
+    while char != 'EOF' and not final(estado) and estado != '-1':
+        estado = move(estado, char)
+        char = prox_char()
+
+        if print_estado:
+            print(f'O estado atual é: {estado}')
+            print(f'O caractere atual é: {char}')
+
+    if final(estado):
+        print('Cadeia aceita')
+    else:
+        print('Cadeia rejeitada')
 
 
 if __name__ == '__main__':
     abre_arquivo('testes/teste01.txt')
+
+    getToken()
 
     arquivo.close()
---
+++ src/tabela.py
@@ -0,0 +1,21 @@
+tabela_de_simbolos = dict()
+
+# lexema: (tipo_token, valor, tipo_dado)
+
+tabela_de_simbolos['program'] = ('program', '', '')
+tabela_de_simbolos['begin'] = ('begin', '', '')
+tabela_de_simbolos['end'] = ('end', '', '')
+
+tabela_de_simbolos['int'] = ('int', '', '')
+tabela_de_simbolos['char'] = ('char', '', '')
+tabela_de_simbolos['float'] = ('float', '', '')
+
+tabela_de_simbolos['if'] = ('if', '', '')
+tabela_de_simbolos['then'] = ('then', '', '')
+tabela_de_simbolos['else'] = ('else', '', '')
+
+tabela_de_simbolos['while'] = ('while', '', '')
+tabela_de_simbolos['repeat'] = ('repeat', '', '')
+tabela_de_simbolos['until'] = ('until', '', '')
+
+# ----
