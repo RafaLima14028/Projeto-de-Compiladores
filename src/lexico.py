3c07f623969b983206f92a77934c39499e07d5a3
Rafael Alves <rafael.alv334@gmail.com>
2023-09-27 11:54:00 -0300

    Nova versão

--- src/lexico.py
+++ src/lexico.py
@@ -1,42 +1,8 @@
 import tabela_de_simbolos as tabela
-from tabela_de_transicao import tabela_transicao, estados_finais
+import tabela_de_simbolos_metodos as funcs
 
 arquivo = None
 nome_id = None
-
-
-def tipo_char(char: str) -> str:
-    if char.isdigit():
-        return 'digito'
-    elif char.isalpha() or char == '_':
-        return 'letra_'
-    elif char == ' ' or char == '\t' or char == '\n':
-        return 'ws'
-    else:
-        return char
-
-
-def abre_arquivo(caminho: str) -> None:
-    global arquivo
-
-    try:
-        arquivo = open(caminho, "r")
-    except FileNotFoundError:  # Arquivo não encontrado
-        print(f'O arquivo {caminho} não foi encontrado.')
-        raise
-    except PermissionError:  # Não tem permissão para abri-lo
-        print(f'Você não tem permissão para acessar esse arquivo')
-        raise
-    except IOError as e:  # Problemas de entrada/saida gerais
-        print(f'Erro de E/S: {e}')
-        raise
-    except Exception as e:
-        print(f'Erro desconecido: {e}')
-        raise
-
-
-def lookhead() -> None:
-    arquivo.seek(arquivo.tell() - 1, 0)
 
 
 def setInt() -> None:
@@ -79,18 +45,105 @@ def setId() -> None:
 
     if item is None:
         tabela.tabela_de_simbolos[item] = ('id', '', '')
+        return None, None, None
         # TODO: RETORNAR OS VALORES
     else:
         if item[0] == 'id':  # Não está reservado, mas está presente na tabela
-            ...
+            return None, None, None
             # TODO: RETORNAR OS VALORES
         else:  # É reservado
-            ...
+            return None, None, None
             # TODO: RETORNAR OS VALORES
 
 
 def setChar() -> None:
     ...
+
+
+estados_finais = [
+    2, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15,
+    17, 20, 22, 24, 27, 31, 32, 33, 35, 36,
+    37, 38, 40
+]
+
+tabela_transicao = {
+    1: {
+        '=': 2,
+        '>': 3,
+        '<': 6,
+        '!': 9,
+        '+': 11,
+        '-': 12,
+        '*': 13,
+        '/': 14,
+        '^': 15,
+        '{': 16,
+        "'": 18,
+        'letra_': 21,
+        'digito': 23,
+        ')': 32,
+        '(': 33,
+        ':': 34,
+        ';': 37,
+        ',': 38,
+        'ws': 39
+    },
+    2: funcs.return_oprel_eq,
+    3: {
+        '=': 4,
+        'outros': 5
+    },
+    4: setId,
+    5: setId,
+    6: {
+        '=': 7,
+        'outros': 8
+    },
+    7: setId,
+    8: setId,
+    21: {
+        'letra_': 21,
+        'digito': 21,
+        'outros': 22
+    },
+    22: setId
+}
+
+
+def tipo_char(char: str) -> str:
+    if char == 'EOF':
+        return char
+    elif char.isdigit():
+        return 'digito'
+    elif char.isalpha() or char == '_':
+        return 'letra_'
+    elif char == ' ' or char == '\t' or char == '\n':
+        return 'ws'
+    else:
+        return char
+
+
+def abre_arquivo(caminho: str) -> None:
+    global arquivo
+
+    try:
+        arquivo = open(caminho, "r")
+    except FileNotFoundError:  # Arquivo não encontrado
+        print(f'O arquivo {caminho} não foi encontrado.')
+        raise
+    except PermissionError:  # Não tem permissão para abri-lo
+        print(f'Você não tem permissão para acessar esse arquivo')
+        raise
+    except IOError as e:  # Problemas de entrada/saida gerais
+        print(f'Erro de E/S: {e}')
+        raise
+    except Exception as e:
+        print(f'Erro desconecido: {e}')
+        raise
+
+
+def lookhead() -> None:
+    arquivo.seek(arquivo.tell() - 1, 0)
 
 
 def prox_char() -> str:
@@ -111,7 +164,7 @@ def final(estado: int) -> bool:
         return False
 
 
-def move(estado: int, char: str) -> str | int:
+def move(estado: int, char: str) -> int:
     tipo_do_char = tipo_char(char)
 
     outros = 'outros'
@@ -129,13 +182,7 @@ def estado_inicial() -> int:
 
 
 def acoes(estado: int) -> None:
-    if final(estado):
-        tipo, valor, eh_lookhead = tabela_transicao[estado]()
-
-        print(tipo, valor)
-
-        if eh_lookhead:
-            lookhead()
+    print(tabela_transicao[estado]())
 
 
 def getToken() -> None:
@@ -159,7 +206,10 @@ def getToken() -> None:
             print(f'O caractere atual é: {char}')
             print()
 
-    if final(estado) and estado != -1:
+    # if char == 'EOF':
+    #     estado = move(estado, char)
+
+    if final(estado):
         print('Cadeia aceita')
         acoes(estado)
     else:
---
+++ src/tabela_de_simbolos_metodos.py
@@ -0,0 +1,2 @@
+def return_oprel_eq() -> (str, str, bool):
+    return 'oprel', 'EQ', True
